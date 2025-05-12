79ade46f0ca8ae89c94144564810f7b424ef1dc0
abdu-personal <abduak5378@gmail.com>
2025-05-12 18:15:59 +0530

    first commit

---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Self-taught MERN stack developer honed skills in creating web
+          applications with a passion. Highly adaptable and proven aptitude for
+          problem-solving and a knack for rapidly assimilating new ideas and
+          technologies. Efficient in managing projects and collaborating
+          effectively, whether working independently or as part of a team.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
